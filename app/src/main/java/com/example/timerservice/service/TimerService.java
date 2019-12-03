7d29d9c1be2f29bae0f496189f9ecbe7e5aebd1b
Husein Muhdhor <huseinmuhdhor97@gmail.com>
2019-12-04 00:34:21 +0700

    first commit

---
+++ app/src/main/java/com/example/timerservice/service/TimerService.java
@@ -0,0 +1,65 @@
+package com.example.timerservice.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.SystemClock;
+import android.util.Log;
+
+public class TimerService extends Service {
+
+    public static final String BROADCAST_ACTION = "com.example.timerservice";
+    long timeInMilliseconds = 0L;
+    private Intent intent;
+    private Handler handler = new Handler();
+    private long initial_time;
+
+    private Runnable sendUpdatesToUI = new Runnable() {
+        public void run() {
+            DisplayLoggingInfo();
+            handler.postDelayed(this, 1000); // 1 seconds
+        }
+    };
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        initial_time = SystemClock.uptimeMillis();
+        intent = new Intent(BROADCAST_ACTION);
+        handler.removeCallbacks(sendUpdatesToUI);
+        handler.postDelayed(sendUpdatesToUI, 1000); // 1 second
+    }
+
+    private void DisplayLoggingInfo() {
+        timeInMilliseconds = SystemClock.uptimeMillis() - initial_time;
+
+        int timer = (int) timeInMilliseconds / 1000;
+        int mins = timer / 60;
+        int secs = timer % 60;
+        intent.putExtra("mins", mins);
+        intent.putExtra("secs", secs);
+
+        Log.i("D3MI03", "" + mins + ":" + String.format("%02d", secs));
+        sendBroadcast(intent);
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacks(sendUpdatesToUI);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        startService(intent);
+        return START_NOT_STICKY;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+}
